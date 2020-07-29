2a02d425553024642a33330154a8b540e94f485f
madoca-git <maca12vel@gmail.com>
2020-07-29 11:34:20 +0900

    ”Basic認証を導入”

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+  
+  private
+  
+  def basic_auth
+    authrntiate_or_request_with_http_basic do |username, password|
+      user_name == 'madoca' && password == '1029'
+    end
+  end
+
 end
