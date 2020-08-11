11dcbf6de8e712d20e0bdbf7a538ead777016bc8
madoca-git <maca12vel@gmail.com>
2020-08-11 11:40:21 +0900

    商品購入機能の実装
    What
    出品されている商品を購入できる機能の実装を行いました。
    
    Why
    商品売買が主な目的のアプリケーションにおいて必須の機能なので。

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,9 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 require('jquery')
+require("../card")
 require('../item_new')
+
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
