ae501ac3354acf79c85340b582551d6414c50d68
madoca-git <maca12vel@gmail.com>
2020-08-11 15:00:30 +0900

    Update card.js

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,7 +1,5 @@
 const pay = () => {
   Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
-  console.log(process.env.PAYJP_PUBLIC_KEY)
-  console.log("test")
   const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
